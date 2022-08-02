c49a682329b8ed3464ff2a7a55e8421a93de6122
Rolland <rolland.cj.wu@gmail.com>
2022-08-02 16:00:54 +0800

    initial

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	//import Counter from "$lib/Counter.svelte";
+
+	let count = 0;	
+
+	function CountUp() {
+		count += 1;
+	}
+</script>
+
+<h1>{count}</h1>
+<button on:click={CountUp}>{count}</button>
+
+<!-- <Counter />
+<Counter /> -->
